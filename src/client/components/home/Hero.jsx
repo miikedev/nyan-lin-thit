9ef148867d5858fb07ae92351dd89a5a724408a9
mikedevv <maungdevv@gmail.com>
2024-06-20 21:55:19 +0700

    fix: home hero section

--- src/client/components/home/Hero.jsx
+++ src/client/components/home/Hero.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Image, Spacer } from '@nextui-org/react'
+import { Image, Spacer, Button } from '@nextui-org/react'
 import { Title, Box, Text } from '@mantine/core'
 import homeHeroImage from '../../../assets/home-hero.jpg'
 const Hero = () => {
@@ -8,11 +8,11 @@ const Hero = () => {
     <section className="w-full flex hero">
       <Spacer x={12} />
       <Box className="relative top-[100px]">
-        <Title className='text-[60px]'>Nyan Lin Thit</Title>
-        <button className="absolute right-[-100px] z-50 py-[10px] px-[40px] bg-primary text-white rounded-[10px] text-[55px]">Analytica</button>
+        <h1 className='text-[60px]'>Nyan Lin Thit</h1>
+        <Button className="absolute right-[-60px] z-50 px-4 py-8 bg-primary text-white w-48 text-3xl" radius='sm'>Analytica</Button>
         <Box className='relative top-[100px]'>
           <Text className='mt-10 mb-3 w-5/6'>It's a non-Textrofit organization that tends to political research, advocacy, and youth development.</Text>
-          <button className='mt-[20px] px-[25px] py-[10px] rounded-full bg-primary text-white'>See more</button>
+          <button className='mt-[20px] px-[20px] py-[8px] rounded-full bg-primary text-white'>See more</button>
         </Box>
       </Box>
       <Box className="">
