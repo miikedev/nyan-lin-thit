290d49d30dc7ce9bba0a0216a4f6b74a36f251f2
mikedevv <maungdevv@gmail.com>
2024-06-24 02:46:58 +0700

    fix: hero button

--- src/client/components/home/Hero.jsx
+++ src/client/components/home/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
       <Spacer x={36} />
       <Box className="relative top-[100px]">
         <h1 className='text-[60px]'>Nyan Lin Thit</h1>
-        <Button className="absolute right-[-60px] z-50 px-4 py-8 bg-primary text-white w-48 text-3xl" radius='sm'>Analytica</Button>
+        <Button className="absolute right-[-60px] z-20 px-4 py-8 bg-primary text-white w-48 text-3xl" radius='sm'>Analytica</Button>
         <Box className='relative top-[100px]'>
           <Text className='mt-10 mb-3 w-5/6'>It's a non-Textrofit organization that tends to political research, advocacy, and youth development.</Text>
           <Button className='mt-[20px] px-[20px] py-[8px] rounded-full bg-primary text-white'>See more</Button>
