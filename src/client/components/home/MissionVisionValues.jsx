cf048619662f3b7c79deae4e8178072b344ae00d
mikedevv <maungdevv@gmail.com>
2024-06-24 23:08:01 +0700

    fix: fix styling

--- src/client/components/home/MissionVisionValues.jsx
+++ src/client/components/home/MissionVisionValues.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
-import { Title, Grid, Box, Text, Avatar } from '@mantine/core'
+import { Title, Grid, Box, Text, Avatar, Space } from '@mantine/core'
 // import { Avatar } from '@nextui-org/react'
 const MissionVisionValues = () => {
   return (
     <>
     <Box className='px-[140px]'>
         <Box w={'full'} className='flex justify-between flex-col items-center'>
-            <Title className='text-center font-semibold text-3xl' mb={5}>Lorem ispam lorem ispam</Title>
+            <Title className='text-center font-semibold text-3xl'>Lorem ispam lorem ispam</Title>
+                            <Space h={30} />
             <Box className='flex w-full justify-between'>
                 <Box className='flex justify-center items-center flex-col gap-4 p-6'>
                             <Avatar radius="sm" src="https://i.pravatar.cc/150?u=a04258114e29026708c" />
