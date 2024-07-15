b437e18395d9f75f15457fead656180a88fe9ea1
mikedevv <maungdevv@gmail.com>
2024-07-15 11:40:53 +0700

    feat: dashboardData.js and Loading.jsx added

---
+++ src/client/apis/dashboardData.js
@@ -0,0 +1,13 @@
+import { fetchDashboards } from "./apiService";
+import { useQuery, useQueryClient } from '@tanstack/react-query';
+export const useDashboardData = () => {
+  console.log('useDashboard')
+  return useQuery({
+    queryKey: [`dashboard`],
+    queryFn: () => fetchDashboards(),
+    onSuccess: (data) => {
+      console.log('resources fetch success!', data)
+    }
+  });
+}
+
