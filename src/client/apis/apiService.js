114bdd46af93ed45da44c9ca5907ea6d9982e80a
mikedevv <maungdevv@gmail.com>
2024-08-27 17:04:09 +0700

    fix: api service for deploy

--- src/client/apis/apiService.js
+++ src/client/apis/apiService.js
@@ -1,65 +1,60 @@
-import axios from 'axios';
+import axios from 'axios';  
 
-// Get the environment variables
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL 
-const IS_PRODUCTION = import.meta.env.ENV === 'production';
+// Get the environment variables  
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;  
+const IS_PRODUCTION = import.meta.env.VITE_ENV === 'production';  
 
-// Create axios instance with the base URL
-export const instance = axios.create({
-  baseURL: API_BASE_URL
-});
+// Create axios instance with the base URL  
+export const instance = axios.create({  
+  baseURL: API_BASE_URL  
+});  
 
-console.log(import.meta.env.VITE_API_BASE_URL)
+// Utility function to construct the URL  
+const constructUrl = (path) => {  
+  const basePath = IS_PRODUCTION ? '' : '/api';  
+  return `${basePath}${path}`;  
+};  
 
-// Fetch resources from the API
-export const fetchResources = async (type, page, category, search) => {
-  console.log(type, page, category, search);
+// Fetch resources from the API  
+export const fetchResources = async (type, page, category, search) => {  
+  const url = search === undefined   
+    ? constructUrl(`/resources/type/${type.toUpperCase()}?page=${page}&limit=6&category=${category || ""}`)  
+    : constructUrl(`/resources/type/${type.toUpperCase()}?page=${page}&limit=6&category=${category || ""}&search=${search}`);  
+  
+  try {  
+    const response = IS_PRODUCTION ? await instance.get(url) : await axios.get(url);  
+    return response.data; // This will include the response data, status, and other information  
+  } catch (error) {  
+    console.error(`Error fetching resources:`, error);  
+    throw error;  
+  }  
+};  
 
-  // Construct the URL depending on the environment
-  const basePath = IS_PRODUCTION ? '' : '/api';
-  const url = search === undefined 
-    ? `${basePath}/resources/type/${type.toUpperCase()}?page=${page}&limit=6&category=${category || ""}`
-    : `${basePath}/resources/type/${type.toUpperCase()}?page=${page}&limit=6&category=${category || ""}&search=${search}`;
+export const fetchDashboards = async (startDate, endDate) => {  
+  try {  
+    const { data } = await axios(constructUrl(`/dashboard`));  
+    return data.dashboards[0];  
+  } catch (error) {  
+    console.log(`Error fetching dashboards:`, error);  
+  }  
+};  
 
-  try {
-    const response = IS_PRODUCTION ? await instance.get(url) : await axios.get(url);
-    return response.data; // This will include the response data, status, and other information
-  } catch (error) {
-    // Handle or throw the error as needed
-    console.error(`Error fetching:`, error);
-    throw error;
-  }
+export const fetchDashboardChart = async (startDate, endDate) => {  
+  const paramString = (startDate !== 'Invalid Date' && endDate !== 'Invalid Date') ? `?startDate=${startDate}&endDate=${endDate}` : '';  
+  try {  
+    const { data } = await axios(constructUrl(`/dashboard/chart${paramString}`));  
+    return data;  
+  } catch (error) {  
+    console.log(`Error fetching dashboard chart:`, error);  
+  }  
+};  
+
+export const fetchDashboardMap = async (startDate, endDate) => {  
+  const paramString = (startDate !== 'Invalid Date' && endDate !== 'Invalid Date') ? `?startDate=${startDate}&endDate=${endDate}` : '';  
+  try {  
+    const { data } = await axios(constructUrl(`/dashboard/map/${paramString}`));  
+    return data;  
+  } catch (error) {  
+    console.log(`Error fetching dashboard map:`, error);  
+  }  
 };
-
-export const fetchDashboards = async(startDate, endDate) => {
-  // console.log('fetch dashboard', paramString)
-
-  try {
-    const {data} = await axios(`/api/dashboard`)
-    return data.dashboards[0];
-  } catch (error) {
-    console.log(error)
-  }
-}
-
-
-export const fetchDashboardChart = async(startDate,endDate) => {
-  const paramString = (startDate != 'Invalid Date' && endDate != 'Invalid Date') ? `?startDate=${startDate}&endDate=${endDate}` : '';
-  try {
-    const {data} = await axios(`/api/dashboard/chart${paramString}`)
-    return data;
-  } catch (error) {
-    console.log(error)
-  }
-}
-
-export const fetchDashboardMap = async(startDate,endDate) => {
-  const paramString = (startDate != 'Invalid Date' && endDate != 'Invalid Date') ? `?startDate=${startDate}&endDate=${endDate}` : '';
-  console.log('paramString', paramString)
-  try {
-    const {data} = await axios(`/api/dashboard/map/${paramString}`)
-    return data;
-  } catch (error) {
-    console.log(error)
-  }
-}
